f536fc66bd972e309d56003d70b98c1b967e3f57
Joseph-Wairimu <josephkamore084@gmail.com>
2022-03-24 16:02:14 +0300

    install whitenoise

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path,register_converter
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 from . import views
 from .models import UrlDateConverter
 register_converter(UrlDateConverter,'date')
 urlpatterns=[
     path('',views.news_of_day,name='newsToday'),
-    path('archives/<date:past_date>/',views.past_days_news,name = 'pastNews')
+    path('archives/<date:past_date>/',views.past_days_news,name = 'pastNews'),
+    path('search/', views.search_results, name='search_results'),
+    path('article/(\d+)',views.article,name ='article')
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- tribune/settings.py
+++ tribune/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'news.apps.NewsConfig',
     'django.contrib.sites',
+    'bootstrap3',
     
     
 ]
@@ -67,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -115,6 +117,8 @@ TIME_ZONE = 'Africa/Nairobi'
 USE_I18N = True
 
 USE_TZ = True
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Static files (CSS, JavaScript, Images)
